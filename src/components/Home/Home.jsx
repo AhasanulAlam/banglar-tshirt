9ba14233e3b5d45c4490c2dbf49d31b45c251d0b
AhasanulAlam <ahasanulalam@gmail.com>
2023-04-08 12:31:04 +0800

    added cart item style

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import TShirt from '../TShirt/TShirt';
+import Cart from '../Cart/Cart';
+import './Home.css';
+
 
 const Home = () => {
     const tshirts = useLoaderData();
-    const {} = tshirts;
     return (
-        <div>
-            <h2>This is Home T-Shirt:  {tshirts.length}</h2>
+        <div className='home-container'>
+            <div className='t-shirts-container'>
+            {
+                tshirts.map(tshirt => <TShirt
+                    key={tshirt._id}
+                    tshirt={tshirt}
+                ></TShirt>)                
+            }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
--- src/components/TShirt/TShirt.jsx
+++ src/components/TShirt/TShirt.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import './TShirt.css';
 
-const TShirt = () => {
+const TShirt = ({tshirt}) => {
+    const{_id, picture, name, price, gender} = tshirt;
     return (
-        <div>
-            
+        <div className='t-shirt'>
+            <img src={picture} alt="" />
+            <h4>{name}</h4>
+            <p> ${price}</p>
+            <button>Buy Now</button>
         </div>
     );
 };
